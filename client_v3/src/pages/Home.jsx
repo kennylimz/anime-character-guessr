037fc2ce4e9efc7d820ced909c71e7b016437f1c
Kenny Li <63720387+kennylimz@users.noreply.github.com>
2025-04-20 18:15:54 -0500

    Update Home.jsx

--- client_v3/src/pages/Home.jsx
+++ client_v3/src/pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
           灵感来源<a href="https://blast.tv/counter-strikle"> BLAST.tv </a>,
           数据来源<a href="https://bgm.tv/"> Bangumi </a>。<br />
           <a href="https://space.bilibili.com/87983557">@作者</a>：“感谢 Bangumi 管理员的优化支持，以及各位网友贡献的代码。感谢大家这段时间的热情和支持。”<br/>
-          角色标签有大佬帮忙持续更新，已可对现有角色标签投票。<br/>
+          角色标签有大佬帮忙持续更新，<b>已可对现有角色标签投票</b>。<br/>
           有好的预设或目录可以B站私信<a href="https://space.bilibili.com/87983557">作者</a>，可能被采纳成为每日推荐或者常驻预设。<br/>
           想加入一个童年回忆的主题……
         </p>
