67ddefb88548c2ac37aac826a17d8950cca284db
Kenny Li <63720387+kennylimz@users.noreply.github.com>
2025-06-08 03:19:46 -0500

    Update Leaderboard.jsx

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -21,7 +21,7 @@ const Leaderboard = ({ defaultExpanded = false }) => {
     <>
       <div className="leaderboard-container">
         <div className="leaderboard-header" onClick={toggleExpand1}>
-          <h3>热门出题角色周榜（5.19-5.25）</h3>
+          <h3>热门出题角色周榜（5.26-6.8）</h3>
           <span>奶龙王朝了😅</span>
           <span className={`expand-icon ${isExpanded1 ? 'expanded' : ''}`}>{isExpanded1 ? '▼' : '▶'}</span>
         </div>
@@ -72,7 +72,7 @@ const Leaderboard = ({ defaultExpanded = false }) => {
       </div>
       <div className="leaderboard-container">
         <div className="leaderboard-header" onClick={toggleExpand2}>
-          <h3>热门出题角色总榜（截至5.25）</h3>
+          <h3>热门出题角色总榜（截至6.8）</h3>
           <span className={`expand-icon ${isExpanded2 ? 'expanded' : ''}`}>{isExpanded2 ? '▼' : '▶'}</span>
         </div>
         {isExpanded2 && (
